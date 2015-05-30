2bb555c5d74a761752ca3213875131f4db4187ec
Iris Cheng <iris.cyo@foxmail.con>
2015-05-30 18:58:41 +0800

    drop junk

--- include/db_info.php
+++
@@ -1,6 +0,0 @@
-<?php
-// config('db_host', SAE_MYSQL_HOST_M);
-// config('db_port', SAE_MYSQL_PORT);
-// config('db_name', SAE_MYSQL_DB);
-// config('db_user', SAE_MYSQL_USER);
-// config('db_passwd', SAE_MYSQL_PASS);
